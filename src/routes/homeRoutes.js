925b58d4da110130ff2b44427733a0ac4e4a90f2
João Pedro <jpteixeiraferreira@gmail.com>
2025-10-21 12:23:18 -0300

    Estruturação inicial da API

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+import homeController from '../controllers/HomeController';
+const router = new Router();
+
+router.get('/', (req, res)=>{
+  homeController.index(req, res);
+});
+
+export default router;
